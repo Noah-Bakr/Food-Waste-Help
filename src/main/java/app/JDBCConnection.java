c67ff3eec4da1812c06e02f60fa839c5667b06bf
S4096467 <s4096467@student.rmit.edu.au>
2024-05-27 13:14:47 +1000

    Updated db names to not cause errors

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -107,7 +107,7 @@ public class JDBCConnection {
             statement.setQueryTimeout(30);
 
             // The Query
-            String query = "SELECT * FROM Personas";
+            String query = "SELECT * FROM Persona";
             
             // Get Result
             ResultSet results = statement.executeQuery(query);
