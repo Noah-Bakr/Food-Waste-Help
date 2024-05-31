0b68fa91291e5135ea929532c73dcccae64271d2
NoahBKR05 <noahbakr@outlook.com>
2024-05-31 21:06:19 +1000

    Nav bar is now globally deployable through website builder object. Color scheme is set, need to fix logo and add dropdowns

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -2,6 +2,7 @@ package app;
 
 import java.util.ArrayList;
 
+import helper.WebsiteElementBuilder;
 import io.javalin.http.Context;
 import io.javalin.http.Handler;
 
@@ -23,6 +24,7 @@ import java.sql.Statement;
  */
 
 public class PageIndex implements Handler {
+    WebsiteElementBuilder nav = new WebsiteElementBuilder();
 
     // URL of this page relative to http://localhost:7001/
     public static final String URL = "/";
@@ -38,6 +40,7 @@ public class PageIndex implements Handler {
 
         // Add some CSS (external file)
         html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
+        html = html + "<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0' />";
         html = html + "</head>";
 
         // Add the body
@@ -45,16 +48,8 @@ public class PageIndex implements Handler {
 
         // Add the topnav
         // This uses a Java v15+ Text Block
-        html = html + """
-            <div class='topnav'>
-                <a href='/'>Homepage</a>
-                <a href='mission.html'>Our Mission</a>
-                <a href='page2A.html'>Sub Task 2.A</a>
-                <a href='page2B.html'>Sub Task 2.B</a>
-                <a href='page3A.html'>Sub Task 3.A</a>
-                <a href='page3B.html'>Sub Task 3.B</a>
-            </div>
-        """;
+        
+        html = html + nav.getNavBar();
 
         // Add header content block
         html = html + """
---
+++ src/main/java/helper/WebsiteElementBuilder.java
@@ -0,0 +1,26 @@
+package helper;
+
+public class WebsiteElementBuilder {
+    public String getNavBar() {
+    String str = """
+            <div class='topnav'>
+                <ul>
+                    <li class='logo'>
+                    <img src='./icons/logo.png' alt='Food Waste Help Logo'>
+                    <span class="material-symbols-outlined">
+                    grocery
+                    </span>
+                    <h2>Food Waste Help</h2>
+                    </li>
+                    <li><a href='/'>Homepage</a></li>
+                    <li><a href='mission.html'>Our Mission</a></li>
+                    <li><a href='page2A.html'>Sub Task 2.A</a></li>
+                    <li><a href='page2B.html'>Sub Task 2.B</a></li>
+                    <li><a href='page3A.html'>Sub Task 3.A</a></li>
+                    <li><a href='page3B.html'>Sub Task 3.B</a></li>
+                </ul>
+            </div>
+        """; 
+        return str;
+    }
+}
