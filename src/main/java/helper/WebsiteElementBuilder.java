59abe18e3943496050504176e7607ef8c2333b7f
Noah-Bakr <s4095646@student.rmit.edu.au>
2024-06-01 19:20:05 +1000

    Created Favicon

--- src/main/java/helper/WebsiteElementBuilder.java
+++ src/main/java/helper/WebsiteElementBuilder.java
@@ -26,6 +26,7 @@ public class WebsiteElementBuilder {
     //Returns extra APIs and elements, such as icons and fonts
     public String getExtraCSS() {
         String str = """
+            <link rel='icon' type='image/x-icon' href='./icons/logo.png'>
             <link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0' />";
             <link rel='preconnect' href='https://fonts.googleapis.com'>";
             <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>";
