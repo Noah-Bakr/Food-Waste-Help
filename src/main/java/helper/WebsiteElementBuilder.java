eeb33f9e68b8a010ea3ea40f4a5650a06c07f5ca
Noah-Bakr <s4095646@student.rmit.edu.au>
2024-06-03 08:36:12 +1000

    removed ";"

--- src/main/java/helper/WebsiteElementBuilder.java
+++ src/main/java/helper/WebsiteElementBuilder.java
@@ -28,9 +28,9 @@ public class WebsiteElementBuilder {
         String str = """
             <link rel='icon' type='image/x-icon' href='./icons/logo.png'>
             
-            <link rel='preconnect' href='https://fonts.googleapis.com'>";
-            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>";
-            
+            <link rel='preconnect' href='https://fonts.googleapis.com'>
+            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+
             <link href='https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap' rel='stylesheet'>
             <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
             """;
