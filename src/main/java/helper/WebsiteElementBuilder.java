e31918d8e924361a128f7446f5be95376ba8ab73
Noah-Bakr <s4095646@student.rmit.edu.au>
2024-06-07 09:28:45 +1000

    Footer formatting milestone

--- src/main/java/helper/WebsiteElementBuilder.java
+++ src/main/java/helper/WebsiteElementBuilder.java
@@ -1,6 +1,20 @@
 package helper;
 //<i class='material-symbols-outlined'>grocery</i>
 public class WebsiteElementBuilder {
+    //Returns extra APIs and elements, such as icons and fonts
+    public String getExtraCSS() {
+        String str = """
+            <link rel='icon' type='image/x-icon' href='./icons/logo.png'>
+            
+            <link rel='preconnect' href='https://fonts.googleapis.com'>
+            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+
+            <link href='https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap' rel='stylesheet'>
+            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+            """;
+            return str;
+        }
+    
     //Navigation properties update globally
     public String getNavBar() {
     String str = """
@@ -22,25 +36,17 @@ public class WebsiteElementBuilder {
         return str;
     }
 
-    //Returns extra APIs and elements, such as icons and fonts
-    public String getExtraCSS() {
-        String str = """
-            <link rel='icon' type='image/x-icon' href='./icons/logo.png'>
-            
-            <link rel='preconnect' href='https://fonts.googleapis.com'>
-            <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-
-            <link href='https://fonts.googleapis.com/css2?family=Comfortaa:wght@300..700&display=swap' rel='stylesheet'>
-            <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-            """;
-            return str;
-        }
-
     //Footer properties update globally
     public String getFooter() {
         String str = """
             <div class='footer'>
-                <p>COSC2803 - Studio Project Starter Code (Apr24)</p>
+                <ul>
+                    <a href='/'>
+                        <li><img src='./icons/logo.png' class='footerimg'></li>
+                        <li><h2>Food Waste Help</h2></li>
+                    </a>
+                    <li><a href='mission.html'>Our Mission</a></li>
+                </ul>
             </div>
         """;
         return str;
